ec6406858a7891478b640e68dc6c1c0630a11c00
alumno05 <alumno05@TPP-LAB05-18.TPP.UPEU.EDU.PE>
2015-03-19 16:00:37 -0500

    Sasuke 16

--- src/principal.java
+++ src/principal.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author alumno05
- */
 public class principal {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hola jhopes");
         System.out.println("Hola lavado");
@@ -21,6 +9,7 @@ public class principal {
         System.out.println("FALTAN VARIOS");
         System.out.println("hello");
         System.out.println("los sistems");
+        System.out.println("Hola Jorge");
     }
     
 }

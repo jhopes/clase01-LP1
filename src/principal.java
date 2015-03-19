9248450f3625e6f0e71825221d6a71b44efef74e
alumno05 <alumno05@TPP-LAB05-04.TPP.UPEU.EDU.PE>
2015-03-19 15:28:33 -0500

    Proyecto Guardado por Alondra

--- src/principal.java
+++ src/principal.java
@@ -16,6 +16,7 @@ public class principal {
     public static void main(String[] args) {
         System.out.println("Hola jhopes");
         System.out.println("Hola lavado");
+        System.out.println("Hola Alondra");
     }
     
 }
